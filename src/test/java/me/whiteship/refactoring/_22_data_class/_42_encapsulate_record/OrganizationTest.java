4916e76a73cf7689367c0466acf7730674fae31d
장유진 <jin94@exotech.kr>
2023-07-10 11:58:38 +0900

    리팩토링42. 레코드 캡슐화하기

---
+++ src/test/java/me/whiteship/refactoring/_22_data_class/_42_encapsulate_record/OrganizationTest.java
@@ -0,0 +1,14 @@
+package me.whiteship.refactoring._22_data_class._42_encapsulate_record;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrganizationTest {
+    @Test
+    void name() {
+        Organization organization = new Organization("keesun", "USA");
+        organization.name();
+    }
+
+}
